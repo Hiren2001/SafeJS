ada37c885342eacf339256cae0019060469f4fcb
Hiren Rathod <95280935+Hiren2001@users.noreply.github.com>
2023-06-15 19:40:15 +0530

    Update safe.js

--- safe.js
+++ safe.js
@@ -5,6 +5,7 @@ const Safe = {
   },
 
   validatePassword: function(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+])[a-zA-Z0-9!@#$%^&*()_+]{8,}$/;
     return passwordRegex.test(password);
   },
 
