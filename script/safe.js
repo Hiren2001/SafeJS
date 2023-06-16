1928d5c0353dd6eadfcbb14b9465dffb5d063b64
Hiren Rathod <95280935+Hiren2001@users.noreply.github.com>
2023-06-16 08:32:37 +0530

    Update safe.js

--- script/safe.js
+++ script/safe.js
@@ -44,10 +44,10 @@ const Safe = {
     return encryptedText;
   },
 
-  decryptText: function(encryptText, key) {
+  decryptText: function(encryptedText, key) {
     let decryptedText = '';
-    for (let i = 0; i < encryptText.length; i++) {
-      const charCode = encryptText.charCodeAt(i);
+    for (let i = 0; i < encryptedText.length; i++) {
+      const charCode = encryptedText.charCodeAt(i);
       const decryptedCharCode = charCode - key;
       decryptedText += String.fromCharCode(decryptedCharCode);
     }
